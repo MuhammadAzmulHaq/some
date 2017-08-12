4e12276ca90c726682887a8062bfbf25bde1fc9f
DESKTOP-VU6N9LT\Azmul <decentazm101@gmail.com>
2017-08-12 10:05:08 +0500

    Setuped Successfully

--- src/config/app.js
+++ src/config/app.js
@@ -1,26 +1,32 @@
 
 //FireBase
 exports.firebaseConfig = {
+	apiKey: "AIzaSyDEGSHmQ7m1TyIzuZtjXK3k1ClHO5fTWkg",
+    authDomain: "togotap.firebaseapp.com",
+    databaseURL: "https://togotap.firebaseio.com",
+    projectId: "togotap",
+    storageBucket: "togotap.appspot.com",
+    messagingSenderId: "430104805788"
 };
 
 
 //App setup
 exports.adminConfig={
-  "appName": "FireAdmin",
-  "slogan":"made with love for a better firebase.",
+  "appName": "Admin Panel",
+  "slogan":"Shakalaka Boomba",
 
   "design":{
     "sidebarBg":"sidebar-1.jpg", //sidebar-1, sidebar-2, sidebar-3
-    "dataActiveColor":"rose", //"purple | blue | green | orange | red | rose"
+    "dataActiveColor":"purple", //"purple | blue | green | orange | red | rose"
     "dataBackgroundColor":"black", // "white | black"
   },
 
   "codeversion": "1.0",
-  "allowedUsers":null, //If null, allow all users, else it should be array of allowd users
+  "allowedUsers":["admin@admin.com"], //If null, allow all users, else it should be array of allowd users
   "fieldBoxName": "Fields",
   "maxNumberOfTableHeaders":5,
   "prefixForJoin":["-event"],
-  "methodOfInsertingNewObjects":"push", //timestamp (key+time) | push - use firebase keys
+  "methodOfInsertingNewObjects":"timestamp", //timestamp (key+time) | push - use firebase keys
   "urlSeparator":"+",
 
 
@@ -32,15 +38,20 @@ exports.adminConfig={
     "maps":["map","latlng"],
     "textarea":["description"],
     "html":["content"],
-    "radio":["radio"],
-    "checkbox":["checkbox"],
-    "dropdowns":["status","dropdowns"],
+    "radio":["radio","room_status"],
+    "checkbox":["checkbox","anemity"],
+    "dropdowns":["status","dropdowns","room_area","room_category","room_persons","room_bed"],
   },
   "optionsForSelect":[
       {"key":"dropdowns","options":["new","processing","rejected","completed"]},
-      {"key":"checkbox","options":["Skopje","Belgrade","New York"]},
+      {"key":"anemity","options":["Flat Screen TV with Cable", "Free Wi-Fi", "Outlet Adapters"]},
       {"key":"status","options":["just_created","confirmed","canceled"]},
-      {"key":"radio","options":["no","maybe","yes"]}
+      {"key":"radio","options":["no","maybe","yes"]},
+	  {"key":"room_status","options":["unpublished","published"]},
+	  {"key":"room_persons","options":["0","1","2","3","4","5","7"]},
+	  {"key":"room_area","options":["UnSelected","224x2224ft","334x334ft","500x500ft"]},
+	  {"key":"room_category","options":["UnSelected","Deluxe Room","Family","The Room"]},
+	  {"key":"room_bed","options":["UnSelected","1 Single size bed","2 Single size bed","1 Double size bed","1 Double size bed and 1 Single size bed","2 Double size bed","5 Single size bed"]}
   ],
 
   "paging":{
@@ -60,30 +71,21 @@ exports.navigation=[
       isIndex:true,
       "path": "",
     },
-    {
-      "link": "fireadmin",
-      "path": "/",
-      "name": "All Data",
-      "icon":"event",
-      "tableFields":["name","description"],
-    },
-    {
-      "link": "push",
-      "path": "",
-      "name": "Push notification",
-      "icon":"speaker_notes",
-      "tableFields":[],
-    }
+	{
+		  "link": "fireadmin",
+		  "path": "rooms/items",
+		  "name": "Rooms",
+		  "icon":"add box",
+		  "tableFields":["room_number","floor_number","room_status"],
+	},
+	{
+		  "link": "fireadmin",
+		  "path": "dummy/items",
+		  "name": "Dummy",
+		  "icon":"location_city",
+		  "tableFields":["name","description"],
+	}
   ];
-
-exports.pushSettings={
-  "pushType":"onesignal", //firebase or onesignal
-  "Firebase_AuthorizationPushKey":"", //Firebase push authorization ket
-  "pushTopic":"news", //Only for firebase push
-  "oneSignal_REST_API_KEY":"",
-  "oneSignal_APP_KEY":"",
-  "included_segments":"Active Users", //Only for onesignal push
-}
 
 exports.userDetails={
 
